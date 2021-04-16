e8ef41a0f579898fd6b4a79e514171f27d1cab4e
furkanbırgul <brgl.furkan@gmail.com>
2021-04-16 16:03:10 -0400

    initial commit

---
+++ src/main/java/com/boutiquerugsmw/BoutiquerugsMwApplication.java
@@ -0,0 +1,13 @@
+package com.boutiquerugsmw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoutiquerugsMwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoutiquerugsMwApplication.class, args);
+	}
+
+}
